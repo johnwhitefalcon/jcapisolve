cb9d4b26be0a3cc89e74960044e5646128ff0678
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2022-10-26 22:46:48 +1100

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,21 @@
+
+import { MongoClient } from 'mongodb'
+//const assert = require('assert');
+//const MongoClient = require('mongodb').MongoClient; 
+const uri = process.env.MONGODB_URI;
+const dbname = process.env.MONGO_DB;
+
+export async function connectToDatabase(){
+  
+//  const client = await new MongoClient(uri);
+const client = await new MongoClient(uri)
+const clientPromise  = await client.connect()
+
+  
+const db = await clientPromise.db(dbname);
+
+return {db}
+
+}
+
+
