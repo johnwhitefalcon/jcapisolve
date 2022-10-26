4993fb44c28c70b5b6c8dfacdee788b6843e6208
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2022-10-26 22:45:14 +1100

    Add files via upload

---
+++ mongapi.js
@@ -0,0 +1,20 @@
+
+
+import { connectToDatabase } from '../../util/mongodb'
+
+export default async function handler(req, res) {
+
+ if (req.method === 'POST') {   
+    const {db} = await connectToDatabase();
+
+    const red = req.body; 
+     
+    const result = await db.collection('mongcollect').insertOne(red);
+
+    const one = res.status(200).json(red)
+    console.log(red)
+ 
+    }
+}
+  
+   
